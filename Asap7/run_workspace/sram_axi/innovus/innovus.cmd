b5ba82487cab23399aa0cc94901fb5cebc850aa7
Dosang2108 <minhsang210805@gmail.com>
2026-08-07 00:34:58 +0900

    update

--- Asap7/run_workspace/sram_axi/innovus/innovus.cmd
+++ Asap7/run_workspace/sram_axi/innovus/innovus.cmd
@@ -1,7 +1,7 @@
 #######################################################
 #                                                     
 #  Innovus Command Logging File                     
-#  Created on Thu Aug  6 23:42:52 2026                
+#  Created on Fri Aug  7 00:27:45 2026                
 #                                                     
 #######################################################
 
@@ -152,30 +152,9 @@ editPin -pin {{s_axi_wdata[31]} {s_axi_wdata[30]} {s_axi_wdata[29]} {s_axi_wdata
 editPin -pin {{s_axi_arid[4]} {s_axi_arid[3]} {s_axi_arid[2]} {s_axi_arid[1]} {s_axi_arid[0]} {s_axi_araddr[31]} {s_axi_araddr[30]} {s_axi_araddr[29]} {s_axi_araddr[28]} {s_axi_araddr[27]} {s_axi_araddr[26]} {s_axi_araddr[25]} {s_axi_araddr[24]} {s_axi_araddr[23]} {s_axi_araddr[22]} {s_axi_araddr[21]} {s_axi_araddr[20]} {s_axi_araddr[19]} {s_axi_araddr[18]} {s_axi_araddr[17]} {s_axi_araddr[16]} {s_axi_araddr[15]} {s_axi_araddr[14]} {s_axi_araddr[13]} {s_axi_araddr[12]} {s_axi_araddr[11]} {s_axi_araddr[10]} {s_axi_araddr[9]} {s_axi_araddr[8]} {s_axi_araddr[7]} {s_axi_araddr[6]} {s_axi_araddr[5]} {s_axi_araddr[4]} {s_axi_araddr[3]} {s_axi_araddr[2]} {s_axi_araddr[1]} {s_axi_araddr[0]} {s_axi_arlen[7]} {s_axi_arlen[6]} {s_axi_arlen[5]} {s_axi_arlen[4]} {s_axi_arlen[3]} {s_axi_arlen[2]} {s_axi_arlen[1]} {s_axi_arlen[0]} {s_axi_arsize[2]} {s_axi_arsize[1]} {s_axi_arsize[0]} {s_axi_arburst[1]} {s_axi_arburst[0]} s_axi_arlock {s_axi_arcache[3]} {s_axi_arcache[2]} {s_axi_arcache[1]} {s_axi_arcache[0]} {s_axi_arprot[2]} {s_axi_arprot[1]} {s_axi_arprot[0]} {s_axi_arqos[3]} {s_axi_arqos[2]} {s_axi_arqos[1]} {s_axi_arqos[0]} {s_axi_arregion[3]} {s_axi_arregion[2]} {s_axi_arregion[1]} {s_axi_arregion[0]} s_axi_arvalid s_axi_arready} -side LEFT -layer M6 -spreadType range -start {2.16 10.16} -end {2.16 960.688} -spreadDirection clockwise -pinWidth 0.128 -pinDepth 0.288 -fixOverlap 1 -honorConstraint 1 -fixedPin 1
 editPin -pin {{s_axi_rid[4]} {s_axi_rid[3]} {s_axi_rid[2]} {s_axi_rid[1]} {s_axi_rid[0]} {s_axi_rdata[31]} {s_axi_rdata[30]} {s_axi_rdata[29]} {s_axi_rdata[28]} {s_axi_rdata[27]} {s_axi_rdata[26]} {s_axi_rdata[25]} {s_axi_rdata[24]} {s_axi_rdata[23]} {s_axi_rdata[22]} {s_axi_rdata[21]} {s_axi_rdata[20]} {s_axi_rdata[19]} {s_axi_rdata[18]} {s_axi_rdata[17]} {s_axi_rdata[16]} {s_axi_rdata[15]} {s_axi_rdata[14]} {s_axi_rdata[13]} {s_axi_rdata[12]} {s_axi_rdata[11]} {s_axi_rdata[10]} {s_axi_rdata[9]} {s_axi_rdata[8]} {s_axi_rdata[7]} {s_axi_rdata[6]} {s_axi_rdata[5]} {s_axi_rdata[4]} {s_axi_rdata[3]} {s_axi_rdata[2]} {s_axi_rdata[1]} {s_axi_rdata[0]} {s_axi_rresp[1]} {s_axi_rresp[0]} s_axi_rlast s_axi_rvalid s_axi_rready} -side BOTTOM -layer M7 -spreadType range -start {10.16 2.16} -end {1095.04 2.16} -spreadDirection clockwise -pinWidth 0.128 -pinDepth 0.288 -fixOverlap 1 -honorConstraint 1 -fixedPin 1
 setPinAssignMode -pinEditInBatch false
-saveDesign ./saved/axi_ram_floorplan_power_pins.enc
-zoomBox -11.20275 101.89925 605.30525 653.80550
-zoomBox 13.80650 205.69850 459.23350 604.45075
-zoomBox 43.70825 285.49050 365.52950 573.58925
-zoomBox 66.12100 343.48675 298.63725 551.63850
-zoomBox 88.63850 401.75400 231.43275 529.58525
-zoomBox 94.01400 415.66375 215.38925 524.32050
-zoomBox 102.46700 437.53725 190.16075 516.04175
-zoomBox 82.72200 403.03700 225.51650 530.86850
-zoomBox 35.86650 321.16675 309.41625 566.05225
-zoomBox 20.19175 290.62900 342.01500 578.72950
-zoomBox -75.49650 104.20875 541.01575 656.11875
-zoomBox -152.38475 -45.58600 700.91925 718.30325
-zoomBox -75.49650 104.20850 541.01600 656.11875
-pan -10.36825 342.40700
-zoomBox -60.38275 322.18125 463.65300 791.30500
-zoomBox -31.60025 412.29225 347.01600 751.23450
-zoomBox -23.44500 449.90875 298.37875 738.00975
-zoomBox -5.61300 532.16225 192.02725 709.09250
-pan -64.17550 580.72950
-zoomBox -61.93225 617.69475 80.86300 745.52700
-zoomBox -74.28725 601.33000 93.70700 751.72075
-zoomBox -55.71225 630.94125 65.66375 739.59875
-zoomBox -35.71800 662.26575 38.82250 728.99550
-zoomBox -30.45300 669.94650 32.90650 726.66675
-zoomBox -21.30950 681.70600 24.46775 722.68650
-pan 16.81850 703.66675
+zoomBox -262.36750 -267.91175 918.66850 789.36750
+zoomBox -516.51075 -588.12825 1118.14150 875.23450
+zoomBox -682.20275 -787.03325 1240.91775 934.57000
+zoomBox -400.52675 -451.50725 988.92800 792.35125
+zoomBox -325.03250 -331.79500 856.00400 725.48475
+saveDesign ./saved/axi_ram_floorplan_power_pins.encexit
