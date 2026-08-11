edc90fdfeec45ff6f8496f58f3338e55433668d4
Dosang2108 <minhsang210805@gmail.com>
2026-08-11 23:41:16 +0900

    update

---
+++ Asap7/run_workspace/Risc_V/genus/fv/riscv_pipeline/read_libs.tcl
@@ -0,0 +1,13 @@
+add_search_path /home/user1/Desktop/asap7/asap7sc7p5t_28/LIB/CCS -library -both
+read_library -liberty -both \
+    /home/user1/Desktop/asap7/asap7sc7p5t_28/LIB/CCS/asap7sc7p5t_AO_LVT_TT_ccs_211120.lib \
+    /home/user1/Desktop/asap7/asap7sc7p5t_28/LIB/CCS/asap7sc7p5t_AO_RVT_TT_ccs_211120.lib \
+    /home/user1/Desktop/asap7/asap7sc7p5t_28/LIB/CCS/asap7sc7p5t_INVBUF_LVT_TT_ccs_220122.lib \
+    /home/user1/Desktop/asap7/asap7sc7p5t_28/LIB/CCS/asap7sc7p5t_INVBUF_RVT_TT_ccs_220122.lib \
+    /home/user1/Desktop/asap7/asap7sc7p5t_28/LIB/CCS/asap7sc7p5t_OA_LVT_TT_ccs_211120.lib \
+    /home/user1/Desktop/asap7/asap7sc7p5t_28/LIB/CCS/asap7sc7p5t_OA_RVT_TT_ccs_211120.lib \
+    /home/user1/Desktop/asap7/asap7sc7p5t_28/LIB/CCS/asap7sc7p5t_SEQ_LVT_TT_ccs_220123.lib \
+    /home/user1/Desktop/asap7/asap7sc7p5t_28/LIB/CCS/asap7sc7p5t_SEQ_RVT_TT_ccs_220123.lib \
+    /home/user1/Desktop/asap7/asap7sc7p5t_28/LIB/CCS/asap7sc7p5t_SIMPLE_LVT_TT_ccs_211120.lib \
+    /home/user1/Desktop/asap7/asap7sc7p5t_28/LIB/CCS/asap7sc7p5t_SIMPLE_RVT_TT_ccs_211120.lib
+
--- Asap7/run_workspace/Risc_V/genus/genus.cmd
+++ Asap7/run_workspace/Risc_V/genus/genus.cmd
@@ -1,6 +1,6 @@
 # Cadence Genus(TM) Synthesis Solution, Version 23.14-s090_1, built Feb 27 2025 10:49:50
 
-# Date: Tue Aug 11 23:26:27 2026
+# Date: Tue Aug 11 23:36:13 2026
 # Host: user1-EliteMini (x86_64 w/Linux 7.0.0-28-generic) (6cores*12cpus*1physical cpu*AMD Ryzen 5 7545U 1024KB)
 # OS:   Ubuntu 26.04 LTS
 
