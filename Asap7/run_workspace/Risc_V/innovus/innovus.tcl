58b8b1139b3ccfdfe9e7c3b48e355b766b99c31e
Dosang2108 <minhsang210805@gmail.com>
2026-08-11 22:07:33 +0700

    update

--- Asap7/run_workspace/Risc_V/innovus/innovus.tcl
+++
@@ -1,8 +0,0 @@
-# Convenience wrapper. Run from Asap7/run_workspace/Risc_V/innovus with:
-#   innovus -stylus -files innovus.tcl
-
-set wrapper_file [file normalize [info script]]
-set wrapper_dir  [file dirname $wrapper_file]
-cd $wrapper_dir
-
-source ./tcl/innovus.tcl
