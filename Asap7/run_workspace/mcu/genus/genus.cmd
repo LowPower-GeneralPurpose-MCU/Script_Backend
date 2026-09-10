dc0eac9d70f451716bbfcbf05a4e430e59833f51
Dosang2108 <minhsang210805@gmail.com>
2026-09-10 17:10:07 +0900

    update

--- Asap7/run_workspace/mcu/genus/genus.cmd
+++ Asap7/run_workspace/mcu/genus/genus.cmd
@@ -1,8 +1,9 @@
 # Cadence Genus(TM) Synthesis Solution, Version 23.14-s090_1, built Feb 27 2025 10:49:50
 
-# Date: Thu Sep 10 14:52:02 2026
+# Date: Thu Sep 10 17:07:34 2026
 # Host: user1-EliteMini (x86_64 w/Linux 7.0.0-31-generic) (6cores*12cpus*1physical cpu*AMD Ryzen 5 7545U 1024KB)
 # OS:   Ubuntu 26.04.1 LTS
 
 source tcl/genus.tcl
+exitt
 exit
